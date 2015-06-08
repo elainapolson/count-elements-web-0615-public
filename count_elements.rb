695f6e3019893cfe195c37203d8b5553a0b94d20
elainapolson <elainapolson@gmail.com>
2015-06-08 09:15:23 -0400

    Done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,7 @@
 def count_elements(array)
-  # code goes here
-end
- 
+  array.each_with_object(Hash.new(0)) do |word, counts|
+    counts[word] += 1
+  end
+end 
+
+# need the element and how many times it appears in the array
